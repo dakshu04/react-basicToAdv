042359dc103181f83cf7c851d9b5d8094d1ddcd3
Daksh Purohit <dakshgrows@gmail.com>
2025-05-03 12:22:25 +0530

    starting with react

---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,9 @@
+function Youtube() {
+    return (
+        <>
+            <h1>returning Youtube</h1>
+        </>
+    )
+}
+
+export default Youtube
---
+++ 01basicvite/src/youtube.jsx
@@ -0,0 +1,8 @@
+function Youtube() {
+    return (
+        <>
+            <h1>This is youtube app</h1>
+        </>
+    )
+}
+export default Youtube
